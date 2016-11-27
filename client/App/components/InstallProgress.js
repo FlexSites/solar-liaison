df26e00d3e4f34d43dda774c874e66918b771cdf
Travis Duncombe <travisduncombe18@gmail.com>
2016-11-26 18:38:39 -0700

    Add native-base and Icons

--- client/App/components/InstallProgress.js
+++ client/App/components/InstallProgress.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Icon } from 'native-base';
 
 const InstallProgress = () => {
   return (
     <View>
       <Text style={{ textAlign: 'left', fontSize: 32 }}>Install Progress</Text>
+      <Icon name='ios-sunny' />
+      <Icon name='md-sunny' />
+      <Icon name='ios-partly-sunny' />
+      <Icon name='md-partly-sunny' />
     </View>
   )
 }
--- client/index.android.js
+++ client/index.android.js
@@ -1,10 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import client from './App/store';
+import HomeView from './App/HomeView';
 import {
   AppRegistry,
   StyleSheet,
@@ -12,41 +9,14 @@ import {
   View
 } from 'react-native';
 
-class Project extends Component {
+class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    return (      
+      <ApolloProvider client={client}>
+        <HomeView />
+      </ApolloProvider>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('Project', () => Project);
+AppRegistry.registerComponent('Project', () => App);
