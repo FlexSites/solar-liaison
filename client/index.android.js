ce89315147e819398ba9cb2592bd1f4a36a3079a
Seth Tippetts <sethtippetts@gmail.com>
2016-11-26 20:28:09 -0700

    Using some seed app

--- client/index.android.js
+++ client/index.android.js
@@ -1,22 +1,4 @@
-import React, { Component } from 'react';
-import { ApolloProvider } from 'react-apollo';
-import client from './App/store';
-import HomeView from './App/HomeView';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import setup from './js/setup';
 
-class App extends Component {
-  render() {
-    return (      
-      <ApolloProvider client={client}>
-        <HomeView />
-      </ApolloProvider>
-    )
-  }
-}
-
-AppRegistry.registerComponent('Project', () => App);
+AppRegistry.registerComponent('NativeStarterKit', setup);
